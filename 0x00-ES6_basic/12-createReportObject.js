4b5945d03717c637d20cea7ea58a42817fd09bb6
Gertrude Seworh <111208672+kaf0@users.noreply.github.com>
2023-06-27 01:57:14 -0400

    12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,13 @@
 export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [departmentName, employees] of Object.entries(employeesList)) {
+    allEmployees[departmentName] = [...employees];
+  }
+
   return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments(employeesList) {
+    allEmployees,
+    getNumberOfDepartments() {
       return Object.keys(employeesList).length;
     },
   };
